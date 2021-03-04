f63c2496229ffe14452e571a6f8404750d97f9c6
apontes77 <alexandrepontesgyn@gmail.com>
2021-03-04 11:24:08 -0300

    rotas novas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,10 @@ import {TestPlanComponent} from 'src/app/test-plan/test-plan.component'
 import {TestCaseComponent} from 'src/app/test-case/test-case.component'
 
 
-const routes: Routes = [{
-path:'',
-component: AppComponent,
-children:[
-  {
-    path: '/testCase',
-    loadChildren: () => import('../app/test-case/test-case.component')
-                      .then(t => t.TestCaseComponent)
-  },
-  {
-    path: '/testPlan',
-    loadChildren: () => import('../app/test-plan/test-plan.component')
-                      .then(t => t.TestPlanComponent)
-  }
-]
-},{path:'testCase', component: TestCaseComponent},
-{path:'testPlan', component: TestPlanComponent}];
+const routes: Routes = [
+  {path:'',component: AppComponent,},
+  {path:'testCase', component: TestCaseComponent},
+  {path:'testPlan', component: TestPlanComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
