a59d1df6c10b9c1d446cc092b756a8031774f687
apontes77 <alexandrepontesgyn@gmail.com>
2021-03-04 13:20:38 -0300

    mais permissoes

--- src/app/test-plan/test-plan.component.ts
+++ src/app/test-plan/test-plan.component.ts
@@ -14,7 +14,7 @@ export class TestPlanComponent implements OnInit {
   formulario: FormGroup
 
   constructor(
-          private formBuilder: FormBuilder, 
+          private formBuilder: FormBuilder,
           private http: HttpClient
 ) { }
 
@@ -32,14 +32,19 @@ export class TestPlanComponent implements OnInit {
     'Access-Control-Allow-Origin':'*',
     'Accept': '*/*',
     'Access-Control-Allow-Headers': 'Accept',
-    'Authorization': `Basic ${environment.token}`
+    'Authorization': `Bearer ${environment.token}`,
+    'client_id':'232018A4-CFE2-41B9-B415-C5982EBCF6CA',
+    'response_type':'Assertion',
+    'scope':'vso.analytics vso.auditlog vso.auditstreams_delete vso.auditstreams_manage vso.code_full vso.connected_server vso.dashboards_manage vso.entitlements vso.environment_manage vso.extension.data_write vso.extension_manage vso.gallery_acquire vso.gallery_manage vso.graph_manage vso.identity_manage vso.loadtest_write vso.machinegroup_manage vso.memberentitlementmanagement_write vso.notification_diagnostics vso.notification_manage vso.packaging_manage vso.profile_write vso.project_manage vso.release_manage vso.securefiles_manage vso.security_manage vso.serviceendpoint_manage vso.symbols_manage vso.taskgroups_manage vso.test_write vso.tokenadministration vso.tokens vso.variablegroups_manage vso.wiki_write vso.work_full',
+    'redirect_uri':'https://wonderful-wave-0bae67a10.azurestaticapps.net/'
+
   }
 
    requestOptions = {
     headers: new HttpHeaders(this.headers)
   }
 
-  onSubmit(){        
+  onSubmit(){
     console.log(this.formulario.value)
      this.http.post(
           'https://dev.azure.com/alexandrequeiroz0023/front-end/_apis/testplan/plans?api-version=6.1-preview.1',
