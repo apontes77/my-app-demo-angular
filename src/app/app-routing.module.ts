3979048044aa3c07b3bbab65de5c21effd421a06
apontes77 <alexandrepontesgyn@gmail.com>
2021-03-04 10:53:03 -0300

    fix: fixing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,26 @@
+import { AppComponent } from './app.component';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {TestPlanComponent} from 'src/app/test-plan/test-plan.component'
 import {TestCaseComponent} from 'src/app/test-case/test-case.component'
 
 
-const routes: Routes = [
-  { path: 'testPlan', component: TestPlanComponent },
-  { path: 'testCase', component: TestCaseComponent },
-  { path: '', redirectTo:'testPlan'}
-];
+const routes: Routes = [{
+path:'',
+component: AppComponent,
+children:[
+  {
+    path: 'testCase',
+    loadChildren: () => import('../app/test-case/test-case.component')
+                      .then(t => t.TestCaseComponent)
+  },
+  {
+    path: 'testPlan',
+    loadChildren: () => import('../app/test-plan/test-plan.component')
+                      .then(t => t.TestPlanComponent)
+  }
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
